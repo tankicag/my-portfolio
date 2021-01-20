05793a8643cbe7e4e5dd6ee0bf2f59183fe31421
tankicag <gonevatankica@gmail.com>
2021-01-20 21:17:43 +0000

    ScrollToTop & Google Analytics

--- js/custom.js
+++ js/custom.js
@@ -248,29 +248,25 @@
   });
 
   // ScrollToTop
-  var mybutton = document.querySelector("#btnScrollToTop");
+  $(document).ready(function () {
+    $(".inner-content").scroll(function () {
+      if ($(this).scrollTop() >= 350) {
+        $("#btnScrollToTop").fadeIn();
+      } else {
+        $("#btnScrollToTop").fadeOut();
+      }
+    });
 
-  // When the user scrolls down 20px from the top of the document, show the button
-  function scrollFunction() {
-    if (
-      document.body.scrollTop > 20 ||
-      document.documentElement.scrollTop > 20
-    ) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
-  }
-  window.onscroll = function () {
-    scrollFunction();
-  };
-  // When the user clicks on the button, scroll to the top of the document
-  function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
-
-  mybutton.addEventListener("click", topFunction);
+    $("#btnScrollToTop").click(function () {
+      $(".inner-content").animate(
+        {
+          scrollTop: 0,
+        },
+        100
+      );
+      return false;
+    });
+  });
 
   // Document Ready
   $(document).ready(function () {
